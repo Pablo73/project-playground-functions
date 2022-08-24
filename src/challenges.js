888e48dbfbabac0bfd2b087049e20d74cc6a0b7f
Pablo Dominguez <pasaxo@gmail.com>
2022-08-24 15:09:19 -0300

    requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return  string.split(' ');
 }
 
 // Desafio 4
