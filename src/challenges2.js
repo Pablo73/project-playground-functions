7914c10511da9846ec42b5316333d5e5cd2ddda3
Pablo Dominguez <pasaxo@gmail.com>
2022-08-27 15:21:29 -0300

    Requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,9 +31,17 @@ function generatePhoneNumber(array) {
 
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC &&
+    lineB < lineA + lineC &&
+    lineC < lineA + lineB &&
+    lineA > Math.abs(lineB - lineC) &&
+    lineB > Math.abs(lineA - lineC) &&
+    lineC > Math.abs(lineA - lineB)
+    ) { return true; }
+  return false;
 }
+
 
 // Desafio 13
 function hydrate() {
